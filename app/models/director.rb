6de5e681f2a9827fa442b552a6d22b50038609f3
DunlapN <DunlapN@users.noreply.github.com>
2017-02-14 13:00:08 -0600

    Finished Going Through the doc, need to check grader to fill in

--- app/models/director.rb
+++ app/models/director.rb
@@ -3,5 +3,6 @@ class Director < ApplicationRecord
   validates :name, :uniqueness => {:scope => :dob}
   validates :name, :presence => true
   #must be present; must be unique in combination with year
-
+  has_many :movies, :class_name => "Movie", :foreign_key => "director_id"
+  
 end
